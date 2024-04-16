a901c96e3d895fe384da6e8f1aee2029352e0cdf
Piyush Raja <piyush.raja@live.com>
2024-04-17 00:39:51 +0530

    added customizations

---
+++ lua/kickstart/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'folke/tokyonight-night.nvim',
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
+  },
+}
